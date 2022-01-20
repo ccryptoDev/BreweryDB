2f8b1a36dadc7f73ad87ed261c034594217c13cf
ccryptoDev <smartwebdev90@gmail.com>
2022-01-20 11:13:54 -0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  filepath: 'src/**/*.js',
+  printWidth: 120,
+  semi: true,
+  tabWidth: 2,
+};
